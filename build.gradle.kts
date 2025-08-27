7c78177e33ba2fc085e072d4781f5f03c8cc90e8
Morgan Koh <delacrixmorgan@pm.me>
2025-08-27 21:05:12 +0200

    Migrate to org.jetbrains.intellij.platform, Bump to v1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,48 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.jvm") version "2.2.0"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
 }
 
 group = "io.dontsayboj.mako"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
-    implementation("com.twelvemonkeys.imageio:imageio-webp:3.10.1")
-    implementation("com.twelvemonkeys.imageio:imageio-core:3.10.1")
+    implementation("com.twelvemonkeys.imageio:imageio-webp:3.12.0")
+    implementation("com.twelvemonkeys.imageio:imageio-core:3.12.0")
+
+    intellijPlatform {
+        intellijIdeaCommunity("2024.1.7")
+        instrumentationTools()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2024.1.7")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+intellijPlatform {
+    pluginConfiguration {
+        name = "Mako Android Drawable Importer"
+        version = project.version.toString()
+        description = "Import and convert drawable resources for Android projects"
+        vendor {
+            name = "Delacrix Morgan"
+            email = "delacrixmorgan@gmail.com"
+            url = "https://github.com/delacrixmorgan/mako-intellij"
+        }
+        ideaVersion {
+            sinceBuild = "241"
+            untilBuild = null
+        }
+    }
 }
 
-tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("231")
-        untilBuild.set("")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
